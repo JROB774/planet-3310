02549ee1fbc727e84ee59ad030d3785e3b3cc5ec
Joshua Robertson <joshrobertson774@gmail.com>
2021-01-30 11:58:10 +0000

    Changed MAINWIN to WINMAIN.

--- build.bat
+++ build.bat
@@ -6,4 +6,4 @@ set BuildMode=debug
 if %BuildMode% == debug set CompileFlags=%CompileFlags% -g -D NK_DEBUG
 
 if not exist bin\win32 mkdir bin\win32
-.tcc\tcc %CompileFlags% -lwinmm -o bin\win32\NOKIA.EXE src\MAINWIN.C
+.tcc\tcc %CompileFlags% -lwinmm -o bin\win32\NOKIA.EXE src\WINMAIN.C
