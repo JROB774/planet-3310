5a0015350902cef0f640194972dcfb500f072f88
Joshua Robertson <joshrobertson774@gmail.com>
2021-02-07 22:43:06 +0000

    Now compiling 32-bit and 64-bit versions for Windows.

--- build.bat
+++ build.bat
@@ -12,12 +12,13 @@ goto error
 
 :win32
 if not exist bin\win32 mkdir bin\win32
-.tcc\tcc %CompileFlags% -D NK_DEBUG -lwinmm -o bin\win32\NOKIA.EXE src\WIN32.C
+.tcc\tcc %CompileFlags% -m32 -lwinmm -o bin\win32\SPACE-32.EXE src\WIN32.C res\icon32.res
+.tcc\tcc %CompileFlags% -m64 -lwinmm -o bin\win32\SPACE-64.EXE src\WIN32.C res\icon64.res
 goto quit
 
 :web
 if not exist bin\web mkdir bin\web
-emcc src\WEB.C -s ALLOW_MEMORY_GROWTH=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 --preload-file bin\web\SNDBANK -o bin\web\NOKIA.html
+emcc src\WEB.C -s ALLOW_MEMORY_GROWTH=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 --preload-file bin\web\SNDBANK -o bin\web\SPACE.html
 goto quit
 
 :error
--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 
 pushd bin\win32
-NOKIA.EXE
+SPACE-64.EXE
 popd
